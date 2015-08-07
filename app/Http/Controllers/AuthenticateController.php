077741466b5e58c95a522c1423f5d1cf730cb7cb
Ayse Ozcan <ayseozcan1@Ayses-MacBook-Pro.local>
2015-08-07 12:57:29 +0300

    added password hash on User model for form input on angular, login auth setup

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -41,7 +41,7 @@ class AuthenticateController extends Controller {
 
 		try {
 			// Verify the credentials and create a token for the user
-			if(! $token = JWTAuth::attempt($credentials)) {
+			if(!$token = JWTAuth::attempt($credentials)) {
 				return response()->json(['error' =>'invalid_credentials'], 401);
 			}
 		} catch (JWTException $e) {
--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,13 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 */
 	protected $hidden = ['password', 'remember_token'];
 
+	/**
+	* The password attribute is hashed for any form input
+	*
+	*
+	*/
+	public function setPasswordAttribute($value) {
+		$this->attributes['password'] = bcrypt($value);
+	}
+
 }
