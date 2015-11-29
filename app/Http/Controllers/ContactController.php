3e636d24d9323e63c142756e0eea695bf8000ccb
Cory Flanagan <cflan12@gmail.com>
2015-11-28 18:34:04 -0800

    add all changes for production server

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,9 +8,6 @@ use Illuminate\Http\Request;
 //Mandrill 
 use Mail;
 
-//MailChimp
-use \DrewM\MailChimp\MailChimp;
-
 
 class ContactController extends Controller {
 
@@ -20,7 +17,7 @@ class ContactController extends Controller {
 		$data = json_decode($formData);
 		$email = $data->email;
 
-		Mail::raw('Reflection Fitness Free Consultation Request from:' . $email, function($message)
+		Mail::raw('Reflection Fitness Free Consultation Request from: ' . $email, function($message)
 		{
 			$message->from('Jared@ReflectionFitness.com', 'Reflection Fitness Consultation Request');
 
@@ -28,15 +25,4 @@ class ContactController extends Controller {
 		});
       }
 
-      public function addEmailToList() 
-      {
-      	$MailChimp = new MailChimp(env('MAILCHIMP_APIKEY'));
-      	$listId = env('MAILCHIMP_LIST');
-
-      	$formData = file_get_contents('php://input', true);
-		$data = json_decode($formData);
-		$email = $data->email;
-
-		$result = $MailChimp->listSubscribe($listId, $email);
-      }
  }
